68833519c104be648206c95b58eef16e009f7cf9
Thomas Gaudibert <thomas.gaudibert@gmail.com>
2018-09-15 23:09:09 +0200

    Delete useless code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,7 @@ import {ManageDocumentPage} from "../manage-document/manage-document";
 
 import {MenuController } from 'ionic-angular';
 import { AngularFireObject, AngularFireList } from 'angularfire2/database';
-import * as firebase from 'firebase';
+
 
 import { DatabaseProvider } from '../../providers/database/database';
 import { ImagePicker } from '@ionic-native/image-picker';
@@ -40,9 +40,7 @@ export class HomePage {
         public popoverCtrl: PopoverController,
          public menuctrl: MenuController,
        private _DB: DatabaseProvider,
-        private _ALERT  : AlertController,
-        public imagePicker: ImagePicker,
-      private cropService: Crop) {
+        private _ALERT  : AlertController) {
           this._CONTENT = {
                    nom 			: "Thomas",
                    age 	: "22",
@@ -215,94 +213,14 @@ export class HomePage {
       alert.present();
    }
 
-   uploadImage(imageURI){
-  return new Promise<any>((resolve, reject) => {
-    let storageRef = firebase.storage().ref();
-    let imageRef = storageRef.child('image').child('imageName');
-    this.encodeImageUri(imageURI, function(image64){
-      imageRef.putString(image64, 'data_url')
-      .then(snapshot => {
-        resolve(snapshot.downloadURL)
-      }, err => {
-        reject(err);
-      })
-    })
-  })
-}
 
-encodeImageUri(imageUri, callback) {
-  var c = document.createElement('canvas');
-  var ctx = c.getContext("2d");
-  var img = new Image();
-  img.onload = function () {
-    var aux:any = this;
-    c.width = aux.width;
-    c.height = aux.height;
-    ctx.drawImage(img, 0, 0);
-    var dataURL = c.toDataURL("image/jpeg");
-    callback(dataURL);
-  };
-  img.src = imageUri;
-};
-
-openImagePicker(){
-  this.imagePicker.hasReadPermission().then(
-    (result) => {
-      if(result == false){
-        // no callbacks required as this opens a popup which returns async
-        this.imagePicker.requestReadPermission();
-      }
-      else if(result == true){
-        this.imagePicker.getPictures({
-          maximumImagesCount: 1
-        }).then(
-          (results) => {
-            for (var i = 0; i < results.length; i++) {
-              this.uploadImageToFirebase(results[i]);
-            }
-          }, (err) => console.log(err)
-        );
-      }
-    }, (err) => {
-      console.log(err);
-    });
-  }
+   search(){
 
 
 
-  uploadImageToFirebase(image){
-    //to do method
-  }
+   }
 
 
-
-  openImagePickerCrop(){
-  this.imagePicker.hasReadPermission().then(
-    (result) => {
-      if(result == false){
-        // no callbacks required as this opens a popup which returns async
-        this.imagePicker.requestReadPermission();
-      }
-      else if(result == true){
-        this.imagePicker.getPictures({
-          maximumImagesCount: 1
-        }).then(
-          (results) => {
-            for (var i = 0; i < results.length; i++) {
-              this.cropService.crop(results[i], {quality: 75}).then(
-                newImage => {
-                  this.uploadImageToFirebase(newImage);
-                },
-                error => console.error("Error cropping image", error)
-              );
-            }
-          }, (err) => console.log(err)
-        );
-      }
-    }, (err) => {
-      console.log(err);
-    });
-  }
 
 
 
