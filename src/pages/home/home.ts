390275bffc929ecbc7c5fa706cf32a31f751111d
Thomas Gaudibert <thomas.gaudibert@gmail.com>
2018-09-17 19:10:12 +0200

    delete useless code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -47,11 +47,7 @@ export class HomePage {
        private _DB: DatabaseProvider,
         private _ALERT  : AlertController,
       private modalCtrl: ModalController) {
-          this._CONTENT = {
-                   nom 			: "Thomas",
-                   age 	: "22",
-                   emploi    : "Connard"
-                };
+
 
 
           this.initializeItems();
@@ -67,13 +63,6 @@ export class HomePage {
     ];
   }
 
-
-
-
-  ionViewDidEnter()
-   {
-      this.retrieveCollection();
-   }
 
 
   logOut() {
@@ -93,144 +82,6 @@ export class HomePage {
     });
   }
 
-
-  generateCollectionAndDocument() : void
-   {
-      this._DB.createAndPopulateDocument(this._COLL,
-                                         this._DOC,
-                                         this._CONTENT)
-      .then((data : any) =>
-      {
-         console.dir(data);
-      })
-      .catch((error : any) =>
-      {
-         console.dir(error);
-      });
-   }
-
-
-
-
-   /**
-    * Retrieve all documents from the specified collection using the
-    * getDocuments method of the DatabaseProvider service
-
-
-    */
-   retrieveCollection() : void
-   {
-      this._DB.getDocuments(this._COLL)
-      .then((data) =>
-      {
-
-         // IF we don't have any documents then the collection doesn't exist
-         // so we create it!
-         if(data.length === 0)
-         {
-            this.generateCollectionAndDocument();
-         }
-
-         // Otherwise the collection does exist and we assign the returned
-         // documents to the public property of locations so this can be
-         // iterated through in the component template
-         else
-         {
-            this.locations = data;
-         }
-      })
-      .catch();
-   }
-
-
-
-
-   /**
-    * Navigate to the manage-document component to begin adding a new document
-    *
-    * @public
-    * @method addDocument
-    * @return {none}
-    */
-   addDocument() : void
-   {
-      this.navCtrl.push(ManageDocumentPage);
-   }
-
-
-
-
-   /**
-    * Update a document by passing the data to the manage-document component
-    *
-    * @public
-    * @method updateDocument
-    * @param  obj          {Object}           The document data we wish to update
-    * @return {none}
-    */
-   updateDocument(obj) : void
-   {
-      let params : any = {
-         collection   : this._COLL,
-         location     : obj
-      };
-      this.navCtrl.push(ManageDocumentPage, { record : params, isEdited : true });
-   }
-
-
-
-
-   /**
-    * Delete a document from the Cloud Firestore collection using the
-    * deleteDocument method of the DatabaseProvider service
-    *
-    * @public
-    * @method deleteDocument
-    * @param  obj          {Object}           The document ID for the document we wish to delete
-    * @return {none}
-    */
-   deleteDocument(obj) : void
-   {
-      this._DB.deleteDocument(this._COLL,
-      						obj.id)
-      .then((data : any) =>
-      {
-         this.displayAlert('Success', 'The record ' + obj.city + ' was successfully removed');
-      })
-      .catch((error : any) =>
-      {
-         this.displayAlert('Error', error.message);
-      });
-   }
-
-
-
-
-   /**
-    * Provide feedback to user after an operation has succeeded/failed
-    *
-    * @public
-    * @method displayAlert
-    * @param  title          {String}           Heading for alert message
-    * @param  message        {String}           Content for alert message
-    * @return {none}
-    */
-   displayAlert(title      : string,
-                message    : string) : void
-   {
-      let alert : any     = this._ALERT.create({
-         title      : title,
-         subTitle   : message,
-         buttons    : [{
-          text      : 'Got It!',
-          handler   : () =>
-          {
-            this.retrieveCollection();
-          }
-        }]
-      });
-      alert.present();
-   }
 
 
    search(){
