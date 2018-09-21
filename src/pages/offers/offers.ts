46680fb632f0227cae11cab428ee07573778ad6c
Thomas Gaudibert <thomas.gaudibert@gmail.com>
2018-09-21 11:49:08 +0200

    add firebase offers content

--- src/pages/offers/offers.ts
+++ src/pages/offers/offers.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
+import { DatabaseProvider } from '../../providers/database/database';
+import * as firebase from 'firebase';
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
+import { ImageProvider } from '../../providers/image/image';
 
 /**
  * Generated class for the OffersPage page.
@@ -14,11 +19,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OffersPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    mainList: Array<any>;
+    showSearch: any;
+    public profiles: any;
+
+    constructor(public navCtrl: NavController,
+       public navParams: NavParams,
+       public popoverCtrl: PopoverController,
+       private _IMG         : ImageProvider,
+
+       private _DB          : DatabaseProvider) {
+      this.mainList = [{img: 'assets/img/01.png'},
+      {img: 'assets/img/02.png'},
+      {img: 'assets/img/03.png'},
+      {img: 'assets/img/01.png'},
+      {img: 'assets/img/02.png'},
+      {img: 'assets/img/03.png'}]
+
+      this.loadAndParseProfiles();
+    }
+
+  // searchbar
+    toggleSearch() {
+      this.showSearch =!this.showSearch;
+    }
+
+
+  // like function
+  activeLike(item,$event){
+    $event.stopPropagation();
+    item.like = !item.like;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad OffersPage');
-  }
+  //Filter popover function
+    presentFilterPopover(myEvent) {
+      let popover = this.popoverCtrl.create('Filter');
+      popover.present({
+        ev: myEvent
+      });
+    }
+
+  //goTo function
+    goTo(page){
+      this.navCtrl.push(page);
+    }
+
+    loadAndParseProfiles()
+     {
+        this.profiles = this._DB.renderProfiles();
+
+     }
 
 }
